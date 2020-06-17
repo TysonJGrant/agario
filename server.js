dfa4a2883d77d0c5ea2347600cd6bea84b024edc
Tyson Grant <tyson.grant@uon.edu.au>
2020-06-18 03:36:12 +1000

    added necessary heroku components

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
 let Player = require('./Player.js');
 
-//const io = require('socket.io')(3000)  //for local
-var sock = require('socket.io');
-var express = require("express");
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = socketIo(server);
+const io = require('socket.io')();
 
 const users = {}
 const w = 1000;
@@ -20,7 +10,7 @@ setInterval(function(){
   io.sockets.emit('update_game', users)
 }, 50);
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   socket.on('new-user', name => {
     socket.broadcast.emit('user-connected', name);
     users[socket.id] = new Player(w, h);
